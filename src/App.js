ea7c7a0d1a18b4c2af8a363c326a576b705f1deb
JhowF <jonathanferreirar@gmail.com>
2022-10-18 00:03:16 -0300

    Entendendo_melhor_o_JSX

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'jonathan';
+
   return (
-    <div className="App">
+    <div className="App">  {/*Class no react é className="" */}
      
         <h1> Olá React! </h1>
+        <h2>Olá {name}</h2>
+        <p>Soma: {2+2}</p>
         <p>Meu primeiro App</p>
+          <img src="" alt=""/>  {/*No react uma tag deve ser sempre fechada msm sendo uma img que é uma self close*/}
+          
     </div>
   );
 }
